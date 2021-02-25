aa9207b19097f1700bb333a0b038b992abf8bc25
Jasmine Flores <jflores208@cnm.edu>
2021-02-25 16:01:32 -0700

    adding changes

--- game.js
+++ game.js
@@ -10,3 +10,58 @@ tag on the page and log it to the console.
 // Add that function to the onclick property of
 // the button on the page.
 //Jasmine Flores
+
+
+//document.addEventListener (function (){
+  //  var div= document.createElement('div');
+  //  div.className="feedback";
+
+//}
+
+
+
+let feedback= document.createElement("div");
+feedback.className= "feedback";
+
+
+let container = document.querySelector(".guessGame");
+container.appendChild(feedback);
+
+
+
+
+
+
+function playGame () {
+    
+    let userInputTag = document.querySelector(".guessButton");
+    let userInput = parseInt (userInputTag.value);
+
+    let  min=1;
+    let max=100;
+    let answer = Math.floor(Math.random()*(max-min)+min);
+
+    if (userInput > answer) {
+        feedback.innerText= "oops too high!"
+    }
+
+    else if (userInput < answer) {
+        feedback.innerText= "oops too low"
+    }
+
+    else if (userInput === answer) {
+        feedback.innerText= "you got it!"
+    }
+}
+
+
+
+let button = document.querySelector(".guessButton");
+button.onclick = playGame;
+
+
+
+        
+
+
+
