3a87e2ddeadb4b987301f589d0ea43cf7a18460a
antgarcia12 <antgarcia014@gmail.com>
2021-02-24 19:57:21 -0700

    added functions to game.js

--- game.js
+++ game.js
@@ -11,3 +11,57 @@ tag on the page and log it to the console.
 
 // Add that function to the onclick property of
 // the button on the page.
+
+/*
+Now, let's make the function called when the button is clicked
+actually play the game. First, change the name of
+the function to something more sensible like playGame
+
+Inside that function...
+Make a new variable to store the true answer. This variable
+should have a value of a randomly chosen number between
+1 and 100 (including the values 1 and 100).
+
+Compare the user's guess to the actual answer. Then, modify
+the text inside the feedback div you created as follows:
+
+If the guess is too high, tel
+If the guess is too high, tell the user "Too high"
+If the guess is too low, tell the user "Too low"
+If the guess is right, tell the user they've won!
+
+Note: right now, your game will generate a new guess
+each time the user clicks the button. That's -okay-
+for now, though soon we'll make changes so that the user's
+guesses all check against the same correct answer
+*/
+
+
+let feedback = document.createElement("div");
+feedback.className = "feedback";
+let container = document.querySelector(".guessGame");
+container.appendChild(feedback);
+
+function playGame() {
+    let userInputTag = document.querySelector(".guessInput");
+    let userInput = parseInt(userInputTag.value); 
+
+    let min = 1;
+    let max = 100;
+    let answer = Math.floor(Math.random() * (max - min) + min);
+
+    if (userInput > answer) {
+        feedback.innerText = "Too high, bro!"
+    }
+
+    else if (userInput < answer) {
+        feedback.innerText = "Too low, bro!"
+    }
+    else if (userInput === answer) {
+        feedback.innerText = "NICE ^.^"
+    }
+}
+
+let button = document.querySelector(".guessButton");
+button.onclick = playGame;
+
