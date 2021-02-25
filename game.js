5b5ccfc908951560f2b4433b2b5c26a70af1de92
Vannah-22 <vannjackson16@gmail.com>
2021-02-24 17:50:09 -0700

    adding div and created class

--- game.js
+++ game.js
@@ -1,12 +1,23 @@
-Vanessa
 // Create a new div
+let feedback = document.createElement("div");
+
 // Give this new div a class of "feedback"
 // Append this new div to the div with class "guessGame"
+feedback.append(guessGame);
 
 /* Create a function that will run when the button is clicked.
 When this function runs, it will take the value of the input
 tag on the page and log it to the console.
 */
 
+function Button();
+    return 
+
 // Add that function to the onclick property of
 // the button on the page.
+
+
+document.getElementsByClassName(feedback);
+appendChild(guessGame);
+
+
