ea88b837393ba5b0bf2341cf4a9a9811dbfa639e
d-tenorio <dtenorio.work@gmail.com>
2021-02-24 17:16:08 -0700

    added instructions

---
+++ game.js
@@ -0,0 +1,11 @@
+// Create a new div
+// Give this new div a class of "feedback"
+// Append this new div to the div with class "guessGame"
+
+/* Create a function that will run when the button is clicked.
+When this function runs, it will take the value of the input
+tag on the page and log it to the console.
+*/
+
+// Add that function to the onclick property of
+// the button on the page.
