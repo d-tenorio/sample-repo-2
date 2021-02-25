fc7505619ce0e33175362705be25ac051ac5befc
Vannah-22 <vannjackson16@gmail.com>
2021-02-24 18:02:16 -0700

    updated correct and incorrect answers to prompt

--- game.js
+++ game.js
@@ -2,22 +2,23 @@
 let feedback = document.createElement("div");
 
 // Give this new div a class of "feedback"
+        // feedback.className = "feedback";
+
 // Append this new div to the div with class "guessGame"
-feedback.append(guessGame);
+    // first, get that element
+            // let container = document.querySelector(".guessGame");
+    // then, append it.
+            //container.appendChild(feedback);
+document.guessGame.appendChild(feedback);
 
 /* Create a function that will run when the button is clicked.
 When this function runs, it will take the value of the input
 tag on the page and log it to the console.
 */
-
-function Button();
-    return 
+function Button(guessInput) {
+    console.log(guessInput);
+}
 
 // Add that function to the onclick property of
 // the button on the page.
-
-
-document.getElementsByClassName(feedback);
-appendChild(guessGame);
-
-
+document.onclick.appendChild(Button);
