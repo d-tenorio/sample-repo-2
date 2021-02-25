413b08b0c7b4f9d7bf4b8c94bc6dd62e0b1beafb
Vannah-22 <vannjackson16@gmail.com>
2021-02-24 18:15:22 -0700

    corrected answers and notes

--- game.js
+++ game.js
@@ -6,19 +6,29 @@ let feedback = document.createElement("div");
 
 // Append this new div to the div with class "guessGame"
     // first, get that element
-            // let container = document.querySelector(".guessGame");
+            let container = document.querySelector(".guessGame");
     // then, append it.
-            //container.appendChild(feedback);
-document.guessGame.appendChild(feedback);
+            container.appendChild(feedback);
+// THIS IS WRONG // document.guessGame.appendChild(feedback);
 
 /* Create a function that will run when the button is clicked.
 When this function runs, it will take the value of the input
 tag on the page and log it to the console.
 */
-var theButton = new Object();
-function theButton.onclick("input") {
-    console.log();
-}
+//this function takes in no inputs
+    function logValue() {
+        // get the value of the input tag on the page
+        let userInputTag = document.querySelector(".guessInput");
+        // we need to use the value property of the HTML-Element
+        let userInput = userInputTag.value;
+        // then, just log it (FOR NOW)
+        console.log(userInput);
+    }
+// THIS IS ALSO WRONG // 
+//var Button = new Object();
+// function Button.onclick("guessInput") {
+//     console.log("guessInput");
+// }
 
 // Add that function to the onclick property of
 // the button on the page.
